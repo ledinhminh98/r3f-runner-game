af175665f97c46f4a12095e7bea4f59095144705
ledinhminh <ldminh8398@gmail.com>
2022-12-05 09:27:42 +0700

    Create level

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,5 +1,7 @@
+import Level from "@Components/Level";
 import Light from "@Components/Light";
 import { OrbitControls } from "@react-three/drei";
+import { Debug, Physics } from "@react-three/rapier";
 import React from "react";
 
 interface Props {}
@@ -8,26 +10,11 @@ const Experience: React.FC<Props> = () => {
   return (
     <>
       <OrbitControls makeDefault />
-      <Light />
-      <mesh castShadow position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
-
-      <mesh castShadow position-x={2} scale={1.5}>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
-
-      <mesh
-        receiveShadow
-        position-y={-1}
-        rotation-x={-Math.PI * 0.5}
-        scale={10}
-      >
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
+      <Physics>
+        <Debug />
+        <Light />
+        <Level />
+      </Physics>
     </>
   );
 };
---
+++ src/components/Level/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import * as THREE from "three";
+
+interface Props {}
+
+THREE.ColorManagement.legacyMode = false;
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
+const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+
+const BlockStart = ({ position = [0, 0, 0] }) => {
+  return (
+    <group position={position}>
+      <mesh
+        geometry={boxGeometry}
+        material={floor1Material}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      />
+    </group>
+  );
+};
+
+const Level: React.FC<Props> = () => {
+  return (
+    <>
+      <BlockStart position={[0, 0, 0]} />
+    </>
+  );
+};
+
+export default Level;
