007c12140d0aa2ca33c089b9061bcc728c0d4ca4
Minh <ldminh8398@gmail.com>
2022-12-06 06:28:33 +0700

    Update level

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { useFrame } from "@react-three/fiber";
+import { RigidBody } from "@react-three/rapier";
+import React, { useRef } from "react";
 import * as THREE from "three";
 
 interface Props {}
@@ -24,10 +26,50 @@ const BlockStart = ({ position = [0, 0, 0] }) => {
   );
 };
 
+const BlockSpinner = ({ position = [0, 0, 0] }) => {
+  const obstacle = useRef();
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+
+    const rotation = new THREE.Quaternion();
+    rotation.setFromEuler(new THREE.Euler(0, time, 0));
+    obstacle?.current?.setNextKinematicRotation(rotation);
+  });
+
+  return (
+    <group position={position}>
+      <mesh
+        geometry={boxGeometry}
+        material={floor2Material}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      />
+      <RigidBody
+        ref={obstacle}
+        type="kinematicPosition"
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={obstacleMaterial}
+          scale={[3.5, 0.3, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  );
+};
+
 const Level: React.FC<Props> = () => {
   return (
     <>
-      <BlockStart position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 4]} />
+      <BlockSpinner position={[0, 0, 0]} />
     </>
   );
 };
